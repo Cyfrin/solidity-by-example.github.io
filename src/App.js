8ed4cb122e43791623186a14052d13807afd1494
t4sk <tsk.nakamura@gmail.com>
2019-02-06 10:00:18 +0900

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from './Home'
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     )
   }
 }
