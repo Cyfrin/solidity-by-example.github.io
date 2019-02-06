b892e6f56f40ab3e5e8fb84b064686136b011c8e
t4sk <tsk.nakamura@gmail.com>
2019-02-06 19:13:58 +0900

    style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import './App.css'
 import Home from './Home'
 import HelloWorld from './examples/HelloWorld'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/hello-world" component={HelloWorld} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/hello-world" component={HelloWorld} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
     )
   }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import './Home.css'
 import logo from './logo.svg'
 import Footer from './Footer'
 
 class Home extends Component {
   render() {
     return (
-      <div id="intro">
-        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-          <img src={logo} alt="logo" width="120px" />
-          <h2><a href="./">Solidity by Example</a></h2>
+      <div>
+        <div className="Home-header">
+          <a href="/">
+            <img src={logo} alt="logo" className="Home-logo" />
+          </a>
+          <h2><a href="/">Solidity by Example</a></h2>
         </div>
         <p>
           <em>Solidity by Example</em> is a hands-on introduction
