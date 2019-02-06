c5348b484224dbb4b63c2ea4fa0f6e3b3a4e9cbd
t4sk <tsk.nakamura@gmail.com>
2019-02-06 19:38:17 +0900

    app css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css'
+import Header from './Header'
 import Home from './Home'
 import HelloWorld from './examples/HelloWorld'
 
@@ -8,12 +9,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <Switch>
-            <Route path="/hello-world" component={HelloWorld} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </Router>
+        <Header />
+        <div className="App-main">
+          <Router>
+            <Switch>
+              <Route path="/hello-world" component={HelloWorld} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </Router>
+        </div>
       </div>
     )
   }
