209a6a1c091fd5d384034bddd5952369a3d2d02f
t4sk <tsk.nakamura@gmail.com>
2019-02-06 19:51:48 +0900

    style home

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
 import './Home.css'
-import logo from './logo.svg'
 import Footer from './Footer'
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <div className="Home-header">
-          <a href="/">
-            <img src={logo} alt="logo" className="Home-logo" />
-          </a>
-          <h2><a href="/">Solidity by Example</a></h2>
+      <div className="Home">
+        <h1 className="Home-header">
+          <a href="/">Solidity by Example</a>
+        </h1>
+        <div className="Home-main">
+          <p>
+            <em>Solidity by Example</em> is an introduction
+            to <a href="https://solidity.readthedocs.io">Solidity</a> using example programs.
+          </p>
+
+          <ul>
+            <li><a href="/hello-world">Hello World</a></li>
+          </ul>
         </div>
-        <p>
-          <em>Solidity by Example</em> is a hands-on introduction
-          to <a href="https://solidity.readthedocs.io">Solidity</a> using example programs.
-        </p>
 
-        <ul>
-
-          <li><a href="/hello-world">Hello World</a></li>
-
-        </ul>
         <Footer />
       </div>
     )
