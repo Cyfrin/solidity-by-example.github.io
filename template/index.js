51ce23639b57f8ec9277d3f16d18a47078f76199
t4sk <tsk.nakamura@gmail.com>
2019-02-12 22:20:35 +0900

    unescape double quotes

--- template/index.js
+++ template/index.js
@@ -7,10 +7,14 @@ const readdir = util.promisify(fs.readdir)
 const mustache = require('mustache')
 const marked = require('marked')
 
-async function findSolidityFile(dir) {
+async function findSolidityFiles(dir) {
   const files = await readdir(dir)
 
-  return files.find(file => file.split(".").pop() == "sol")
+  return files.filter(file => file.split(".").pop() == "sol")
+}
+
+function removeExt(fileName) {
+  return fileName.split(".").slice(0, -1).join("")
 }
 
 // convert index.md.mustache -> index.html.js
@@ -18,13 +22,17 @@ async function main() {
   const args = process.argv.slice(2)
   const dir = args[0] || '.'
 
-  const solidityFileName = await findSolidityFile(dir)
+  const solidityFileNames = await findSolidityFiles(dir)
 
-  const code = (await readFile(path.join(dir, solidityFileName))).toString()
+  let codes = {}
+  for (let fileName of solidityFileNames) {
+    codes[removeExt(fileName)] = (await readFile(path.join(dir, fileName))).toString()
+  }
+
   const mdTemplate = (await readFile(path.join(dir, 'index.md.mustache'))).toString()
 
-  const markdown = mustache.render(mdTemplate, { code })
-  const html = marked(markdown)
+  const markdown = mustache.render(mdTemplate, codes)
+  const html = marked(markdown).replace(/&quot;/g, `"`)
 
   const jsTemplate = (await readFile(path.join(__dirname, './index.html.js.mustache'))).toString()
   const js = mustache.render(jsTemplate, { html })
