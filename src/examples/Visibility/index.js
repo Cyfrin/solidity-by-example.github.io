8158ebf3dd186127bf3cd1009b679fa1c40ca69d
t4sk <tsk.nakamura@gmail.com>
2019-02-23 14:59:55 +0900

    visibility

---
+++ src/examples/Visibility/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import Example from "../../Example"
+import html from "./index.html.js"
+
+class Visibility extends Component {
+  render() {
+    return <Example title="Visibility" html={html} />
+  }
+}
+
+export default Visibility
