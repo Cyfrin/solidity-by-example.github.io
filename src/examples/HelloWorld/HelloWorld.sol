34620dd3d4c2ca04f75a738273ccfe6c07878331
t4sk <tsk.nakamura@gmail.com>
2019-07-23 15:19:21 +0900

    edit comments in HelloWorld

--- src/examples/HelloWorld/HelloWorld.sol
+++ src/examples/HelloWorld/HelloWorld.sol
@@ -1,5 +1,4 @@
 /*
-pragma specifies the compiler version of Solidity.
 This code will not compile with a compiler earlier than version 0.5.1
 and it will also not work on a compiler starting from version 0.6.0
 */
