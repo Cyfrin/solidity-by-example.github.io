a6721b3ed7ca9b7c56d66345ccdc85a55aeff402
t4sk <tsk.nakamura@gmail.com>
2019-07-23 14:50:52 +0900

    fix comments on HelloWorld

--- src/examples/HelloWorld/HelloWorld.sol
+++ src/examples/HelloWorld/HelloWorld.sol
@@ -1,6 +1,6 @@
 /*
-pragma specifies what versions of Solidity this file can work on.
-This file will not compile with a compiler earlier than version 0.5.3
+pragma specifies the compiler version of Solidity.
+This code will not compile with a compiler earlier than version 0.5.1
 and it will also not work on a compiler starting from version 0.6.0
 */
 pragma solidity ^0.5.1;
