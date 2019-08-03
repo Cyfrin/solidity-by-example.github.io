3e8e20810d038b89dc3dd254c94b9dfd8106f68a
t4sk <tsk.nakamura@gmail.com>
2019-08-03 19:14:55 +0900

    simplify HelloWorld

--- src/examples/HelloWorld/index.html.js
+++ src/examples/HelloWorld/index.html.js
@@ -1,14 +1,9 @@
 const html = `<p><code>pragma</code> specifies the compiler version of Solidity.</p>
-<pre><code class="language-solidity">/*
-This code will not compile with a compiler earlier than version 0.5.1
-and it will also not work on a compiler starting from version 0.6.0
-*/
+<pre><code class="language-solidity">// compiler version must be greater than 0.5.0 and less than 0.6.0
 pragma solidity ^0.5.1;
 
 contract HelloWorld {
-    function hello() public returns (string memory) {
-        return "Hello World!";
-    }
+  string public greet = "Hello World!";
 }
 </code></pre>
 `
