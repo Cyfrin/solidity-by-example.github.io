18f31177efa83590056466679330c5ffe3f16e42
t4sk <tsk.nakamura@gmail.com>
2019-08-12 07:33:19 +0900

    function component App

--- src/App.js
+++ src/App.js
@@ -42,67 +42,65 @@ import MultiSigWallet from "./examples/MultiSigWallet"
 import Dividend from "./examples/Dividend"
 import PaymentChannel from "./examples/PaymentChannel"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <div className="App-main">
-          <Router basename={process.env.PUBLIC_URL}>
-            <Switch>
-              <Route path="/hello-world" component={HelloWorld} />
-              <Route path="/state-variables" component={StateVariables} />
-              <Route path="/import" component={Import} />
-              <Route path="/safe-math" component={SafeMath} />
-              <Route path="/events" component={Events} />
-              <Route path="/error" component={Error} />
-              <Route path="/sending-ether" component={SendingEther} />
-              <Route path="/payable" component={Payable} />
-              <Route path="/fallback" component={Fallback} />
-              <Route path="/fallback-gas-limit" component={FallbackGasLimit} />
-              <Route path="/re-entrancy" component={ReEntrancy} />
-              <Route path="/self-destruct" component={SelfDestruct} />
-              <Route path="/function" component={Function} />
-              <Route path="/function-modifier" component={FunctionModifier} />
-              <Route
-                path="/view-and-pure-functions"
-                component={ViewAndPureFunctions}
-              />
-              <Route path="/inheritance" component={Inheritance} />
-              <Route
-                path="/shadowing-inherited-state-variables"
-                component={Shadow}
-              />
-              <Route path="/constructor" component={Constructor} />
-              <Route path="/super" component={Super} />
-              <Route path="/visibility" component={Visibility} />
-              <Route path="/loop" component={Loop} />
-              <Route path="/enum" component={Enum} />
-              <Route path="/mapping" component={Mapping} />
-              <Route path="/array" component={Array} />
-              <Route path="/structs" component={Structs} />
-              <Route path="/new-contract" component={NewContract} />
-              <Route path="/calling-contract" component={CallingContract} />
-              <Route path="/call" component={Call} />
-              <Route path="/delegatecall" component={Delegatecall} />
-              <Route path="/library" component={Library} />
-              <Route path="/wei" component={Wei} />
-              <Route path="/gas" component={Gas} />
-              <Route path="/hashing" component={Keccak256} />
-              <Route path="/signature" component={Signature} />
-              <Route path="/multi-sig-wallet" component={MultiSigWallet} />
-              <Route path="/dividend" component={Dividend} />
-              <Route path="/payment-channel" component={PaymentChannel} />
-              <Route component={Home} />
-            </Switch>
-          </Router>
-        </div>
-        <div className="App-footer">
-          <Footer />
-        </div>
+function App(props) {
+  return (
+    <div className="App">
+      <Header />
+      <div className="App-main">
+        <Router basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path="/hello-world" component={HelloWorld} />
+            <Route path="/state-variables" component={StateVariables} />
+            <Route path="/import" component={Import} />
+            <Route path="/safe-math" component={SafeMath} />
+            <Route path="/events" component={Events} />
+            <Route path="/error" component={Error} />
+            <Route path="/sending-ether" component={SendingEther} />
+            <Route path="/payable" component={Payable} />
+            <Route path="/fallback" component={Fallback} />
+            <Route path="/fallback-gas-limit" component={FallbackGasLimit} />
+            <Route path="/re-entrancy" component={ReEntrancy} />
+            <Route path="/self-destruct" component={SelfDestruct} />
+            <Route path="/function" component={Function} />
+            <Route path="/function-modifier" component={FunctionModifier} />
+            <Route
+              path="/view-and-pure-functions"
+              component={ViewAndPureFunctions}
+            />
+            <Route path="/inheritance" component={Inheritance} />
+            <Route
+              path="/shadowing-inherited-state-variables"
+              component={Shadow}
+            />
+            <Route path="/constructor" component={Constructor} />
+            <Route path="/super" component={Super} />
+            <Route path="/visibility" component={Visibility} />
+            <Route path="/loop" component={Loop} />
+            <Route path="/enum" component={Enum} />
+            <Route path="/mapping" component={Mapping} />
+            <Route path="/array" component={Array} />
+            <Route path="/structs" component={Structs} />
+            <Route path="/new-contract" component={NewContract} />
+            <Route path="/calling-contract" component={CallingContract} />
+            <Route path="/call" component={Call} />
+            <Route path="/delegatecall" component={Delegatecall} />
+            <Route path="/library" component={Library} />
+            <Route path="/wei" component={Wei} />
+            <Route path="/gas" component={Gas} />
+            <Route path="/hashing" component={Keccak256} />
+            <Route path="/signature" component={Signature} />
+            <Route path="/multi-sig-wallet" component={MultiSigWallet} />
+            <Route path="/dividend" component={Dividend} />
+            <Route path="/payment-channel" component={PaymentChannel} />
+            <Route component={Home} />
+          </Switch>
+        </Router>
       </div>
-    )
-  }
+      <div className="App-footer">
+        <Footer />
+      </div>
+    </div>
+  )
 }
 
 export default App
