cd774bc1b843e5267725380f057974f89caad58e
t4sk <tsk.nakamura@gmail.com>
2019-09-16 08:55:26 +0900

    Updates

---
+++ precache-manifest.c95d1e182518fb3f0ab0ef3255d2e30f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b8a3fdc7b306a4e8e22a9b722794a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eba8e2cc75f45af9ca3",
+    "url": "/static/css/2.706b70fc.chunk.css"
+  },
+  {
+    "revision": "1253d8702d494bbfc3d4",
+    "url": "/static/css/main.6db4fe6c.chunk.css"
+  },
+  {
+    "revision": "9eba8e2cc75f45af9ca3",
+    "url": "/static/js/2.2e4f1ebe.chunk.js"
+  },
+  {
+    "revision": "1253d8702d494bbfc3d4",
+    "url": "/static/js/main.202df88f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da793b471bc2bf44b0340d93ec74ecb3",
+    "url": "/static/media/logo.da793b47.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.263d97fabac9a4c93d5282b7401a9c0c.js"
+  "/precache-manifest.c95d1e182518fb3f0ab0ef3255d2e30f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
