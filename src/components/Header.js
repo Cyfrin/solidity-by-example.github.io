bb498b8e795998ef294833e916326a8154861d29
t4sk <tsk.nakamura@gmail.com>
2019-09-17 15:14:03 +0900

    style header color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react"
+import React from "react"
 import "./Header.css"
 import logo from "./logo.svg"
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="Header">
-        <a href="/">
-          <img src={logo} alt="logo" className="Header-logo" />
-        </a>
+function Header(props) {
+  return (
+    <div className="Header">
+      <a href="/">
+        <img src={logo} alt="logo" className="Header-logo" />
+      </a>
 
-        <h3>
-          <a href="/">
-            <div>Solidity by Example</div>
-            <div style={{ color: "lightgrey" }}>version 0.5.3</div>
-          </a>
-        </h3>
-      </div>
-    )
-  }
+      <h3>
+        <a href="/">
+          <div>Solidity by Example</div>
+          <div>version 0.5.3</div>
+        </a>
+      </h3>
+    </div>
+  )
 }
 
 export default Header
