0eec03b5903636c2376760f19e9a1f90c3f85fd4
t4sk <tsk.nakamura@gmail.com>
2020-01-11 15:52:55 +0900

    structs

--- src/pages/Structs/index.html.js
+++ src/pages/Structs/index.html.js
@@ -11,27 +11,43 @@ contract Todos {
     // An array of &#39;Todo&#39; structs
     Todo[] public todos;
 
-    function create(string memory text) public {
+    function create(string memory _text) public {
+        // 3 ways to initialize a struct
+        // - calling it like a function
+        todos.push(Todo(_text, false));
+
+        // key value mapping
         todos.push(Todo({
-            text: text,
+            text: _text,
             completed: false
         }));
-    }
 
-    function toggleCompleted(uint index) public {
-        Todo storage todo = todos[index];
-        todo.completed = !todo.completed;
+        // initialize an emoty struct and then update it
+        Todo memory todo;
+        todo.text = _text;
+        // todo.completed initialized to false
+
+        todos.push(todo);
     }
 
     // Solidity automatically created a getter for&#39; todos&#39; so
     // you don&#39;t actually need this function.
-    function getTodo(uint index)
-        public
-        view
+    function get(uint _index) public view
         returns (string memory text, bool completed)
     {
-        Todo storage todo = todos[index];
+        Todo storage todo = todos[_index];
         return (todo.text, todo.completed);
+    }
+
+    // update text
+    function update(uint _index, string memory _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+    // update completed
+    function toggleCompleted(uint _index) public {
+        Todo storage todo = todos[_index];
+        todo.completed = !todo.completed;
     }
 }
 </code></pre>
