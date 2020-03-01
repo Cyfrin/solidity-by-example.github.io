b33c8d7670cc09755a5528950a5237ee8522cfc5
t4sk <tsk.nakamura@gmail.com>
2020-03-01 16:42:16 +0900

    routes

--- src/App.js
+++ src/App.js
@@ -4,43 +4,7 @@ import styles from "./App.module.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
-import HelloWorld from "./pages/HelloWorld"
-import StateVariables from "./pages/StateVariables"
-import SafeMath from "./pages/SafeMath"
-import Payable from "./pages/Payable"
-import SendingEther from "./pages/SendingEther"
-import Fallback from "./pages/Fallback"
-import ReEntrancy from "./pages/ReEntrancy"
-import SelfDestruct from "./pages/SelfDestruct"
-import Events from "./pages/Events"
-import Error from "./pages/Error"
-import Import from "./pages/Import"
-import Function from "./pages/Function"
-import FunctionModifier from "./pages/FunctionModifier"
-import ViewAndPureFunctions from "./pages/ViewAndPureFunctions"
-import Inheritance from "./pages/Inheritance"
-import Shadow from "./pages/Shadow"
-import Constructor from "./pages/Constructor"
-import Super from "./pages/Super"
-import Visibility from "./pages/Visibility"
-import Loop from "./pages/Loop"
-import Enum from "./pages/Enum"
-import Mapping from "./pages/Mapping"
-import Array from "./pages/Array"
-import Structs from "./pages/Structs"
-import NewContract from "./pages/NewContract"
-import CallingContract from "./pages/CallingContract"
-import Call from "./pages/Call"
-import Delegatecall from "./pages/Delegatecall"
-import Library from "./pages/Library"
-import EtherUnits from "./pages/EtherUnits"
-import Gas from "./pages/Gas"
-import Keccak256 from "./pages/Keccak256"
-import Signature from "./pages/Signature"
-import MultiSigWallet from "./pages/MultiSigWallet"
-import Dividend from "./pages/Dividend"
-import PaymentChannel from "./pages/PaymentChannel"
-import MerkleTree from "./pages/MerkleTree"
+import routes from "./routes"
 
 function App(props) {
   return (
@@ -49,49 +13,13 @@ function App(props) {
       <div className={styles.main}>
         <Router basename={process.env.PUBLIC_URL}>
           <Switch>
-            <Route path="/hello-world" component={HelloWorld} />
-            <Route path="/state-variables" component={StateVariables} />
-            <Route path="/import" component={Import} />
-            <Route path="/safe-math" component={SafeMath} />
-            <Route path="/events" component={Events} />
-            <Route path="/error" component={Error} />
-            <Route path="/sending-ether" component={SendingEther} />
-            <Route path="/payable" component={Payable} />
-            <Route path="/fallback" component={Fallback} />
-            <Route path="/re-entrancy" component={ReEntrancy} />
-            <Route path="/self-destruct" component={SelfDestruct} />
-            <Route path="/function" component={Function} />
-            <Route path="/function-modifier" component={FunctionModifier} />
-            <Route
-              path="/view-and-pure-functions"
-              component={ViewAndPureFunctions}
-            />
-            <Route path="/inheritance" component={Inheritance} />
-            <Route
-              path="/shadowing-inherited-state-variables"
-              component={Shadow}
-            />
-            <Route path="/constructor" component={Constructor} />
-            <Route path="/super" component={Super} />
-            <Route path="/visibility" component={Visibility} />
-            <Route path="/loop" component={Loop} />
-            <Route path="/enum" component={Enum} />
-            <Route path="/mapping" component={Mapping} />
-            <Route path="/array" component={Array} />
-            <Route path="/structs" component={Structs} />
-            <Route path="/new-contract" component={NewContract} />
-            <Route path="/calling-contract" component={CallingContract} />
-            <Route path="/call" component={Call} />
-            <Route path="/delegatecall" component={Delegatecall} />
-            <Route path="/library" component={Library} />
-            <Route path="/ether-units" component={EtherUnits} />
-            <Route path="/gas" component={Gas} />
-            <Route path="/hashing" component={Keccak256} />
-            <Route path="/signature" component={Signature} />
-            <Route path="/multi-sig-wallet" component={MultiSigWallet} />
-            <Route path="/dividend" component={Dividend} />
-            <Route path="/payment-channel" component={PaymentChannel} />
-            <Route path="/merkle-tree" component={MerkleTree} />
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
             <Route component={Home} />
           </Switch>
         </Router>
---
+++ src/routes.js
@@ -0,0 +1,190 @@
+import HelloWorld from "./pages/HelloWorld"
+import StateVariables from "./pages/StateVariables"
+import SafeMath from "./pages/SafeMath"
+import Payable from "./pages/Payable"
+import SendingEther from "./pages/SendingEther"
+import Fallback from "./pages/Fallback"
+import ReEntrancy from "./pages/ReEntrancy"
+import SelfDestruct from "./pages/SelfDestruct"
+import Events from "./pages/Events"
+import Error from "./pages/Error"
+import Import from "./pages/Import"
+import Function from "./pages/Function"
+import FunctionModifier from "./pages/FunctionModifier"
+import ViewAndPureFunctions from "./pages/ViewAndPureFunctions"
+import Inheritance from "./pages/Inheritance"
+import Shadow from "./pages/Shadow"
+import Constructor from "./pages/Constructor"
+import Super from "./pages/Super"
+import Visibility from "./pages/Visibility"
+import Loop from "./pages/Loop"
+import Enum from "./pages/Enum"
+import Mapping from "./pages/Mapping"
+import Array from "./pages/Array"
+import Structs from "./pages/Structs"
+import NewContract from "./pages/NewContract"
+import CallingContract from "./pages/CallingContract"
+import Call from "./pages/Call"
+import Delegatecall from "./pages/Delegatecall"
+import Library from "./pages/Library"
+import EtherUnits from "./pages/EtherUnits"
+import Gas from "./pages/Gas"
+import Keccak256 from "./pages/Keccak256"
+import Signature from "./pages/Signature"
+import MultiSigWallet from "./pages/MultiSigWallet"
+import Dividend from "./pages/Dividend"
+import PaymentChannel from "./pages/PaymentChannel"
+import MerkleTree from "./pages/MerkleTree"
+
+const routes = [
+  {
+    path: "/hello-world",
+    component: HelloWorld,
+  },
+  {
+    path: "/state-variables",
+    component: StateVariables,
+  },
+  {
+    path: "/import",
+    component: Import,
+  },
+  {
+    path: "/safe-math",
+    component: SafeMath,
+  },
+  {
+    path: "/events",
+    component: Events,
+  },
+  {
+    path: "/error",
+    component: Error,
+  },
+  {
+    path: "/sending-ether",
+    component: SendingEther,
+  },
+  {
+    path: "/payable",
+    component: Payable,
+  },
+  {
+    path: "/fallback",
+    component: Fallback,
+  },
+  {
+    path: "/re-entrancy",
+    component: ReEntrancy,
+  },
+  {
+    path: "/self-destruct",
+    component: SelfDestruct,
+  },
+  {
+    path: "/function",
+    component: Function,
+  },
+  {
+    path: "/function-modifier",
+    component: FunctionModifier,
+  },
+  {
+    path: "/view-and-pure-functions",
+    component: ViewAndPureFunctions,
+  },
+  {
+    path: "/inheritance",
+    component: Inheritance,
+  },
+  {
+    path: "/shadowing-inherited-state-variables",
+    component: Shadow,
+  },
+  {
+    path: "/constructor",
+    component: Constructor,
+  },
+  {
+    path: "/super",
+    component: Super,
+  },
+  {
+    path: "/visibility",
+    component: Visibility,
+  },
+  {
+    path: "/loop",
+    component: Loop,
+  },
+  {
+    path: "/enum",
+    component: Enum,
+  },
+  {
+    path: "/mapping",
+    component: Mapping,
+  },
+  {
+    path: "/array",
+    component: Array,
+  },
+  {
+    path: "/structs",
+    component: Structs,
+  },
+  {
+    path: "/new-contract",
+    component: NewContract,
+  },
+  {
+    path: "/calling-contract",
+    component: CallingContract,
+  },
+  {
+    path: "/call",
+    component: Call,
+  },
+  {
+    path: "/delegatecall",
+    component: Delegatecall,
+  },
+  {
+    path: "/library",
+    component: Library,
+  },
+  {
+    path: "/ether-units",
+    component: EtherUnits,
+  },
+  {
+    path: "/gas",
+    component: Gas,
+  },
+  {
+    path: "/hashing",
+    component: Keccak256,
+  },
+  {
+    path: "/signature",
+    component: Signature,
+  },
+  {
+    path: "/multi-sig-wallet",
+    component: MultiSigWallet,
+  },
+  {
+    path: "/dividend",
+    component: Dividend,
+  },
+  {
+    path: "/payment-channel",
+    component: PaymentChannel,
+  },
+  {
+    path: "/merkle-tree",
+    component: MerkleTree,
+  },
+]
+
+export default routes
