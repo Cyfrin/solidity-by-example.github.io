6fe610dbd6d03b5ea6b9ec84c597d7c8b48cd87a
t4sk <tsk.nakamura@gmail.com>
2020-03-07 14:47:18 +0900

    home

--- memo/MinimumViableultiSigWallet.sol
+++ memo/MinimumViableultiSigWallet.sol
@@ -1,18 +1,19 @@
+
 pragma solidity ^0.5.11;
 pragma experimental ABIEncoderV2;
 
-// TODO copy paste ECDSA contract here?
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/cryptography/ECDSA.sol";
 
 contract MinimumViableMultiSigWallet {
     using ECDSA for bytes32;
 
+    // TODO nonce?
     address[] public owners;
-    mapping(bytes32 => bool) isExecuted;
+    mapping(bytes32 => bool) public isExecuted;
 
     // NOTE: N / N multisig wallet (does not check if owners are unique)
     constructor(address[] memory _owners) public payable {
-        require(owners.length > 0, "Owners required");
+        require(_owners.length > 0, "Owners required");
         owners = _owners;
     }
 
@@ -21,7 +22,8 @@ contract MinimumViableMultiSigWallet {
     function getTransactionHash(
         address _to,
         uint _value,
-        bytes memory _data
+        bytes memory _data,
+        uint _nonce
     )
         public
         view
@@ -32,8 +34,17 @@ contract MinimumViableMultiSigWallet {
             owners,
             _to,
             _value,
-            _data
+            _data,
+            _nonce
         ));
+    }
+
+    function verify(bytes32 txHash, bytes memory sig) public view returns (address) {
+        return txHash.toEthSignedMessageHash().recover(sig);
+    }
+
+    function getSigHash(bytes32 txHash) public view returns (bytes32){
+        return txHash.toEthSignedMessageHash();
     }
 
     // TODO example (send / withdraw ether and call other contract)
@@ -41,11 +52,12 @@ contract MinimumViableMultiSigWallet {
         address _to,
         uint _value,
         bytes memory _data,
+        uint _nonce,
         bytes[] memory _signatures
     )
         public
     {
-        bytes32 transactionHash = getTransactionHash(_to, _value, _data);
+        bytes32 transactionHash = getTransactionHash(_to, _value, _data, _nonce);
 
         // NOTE: this guards against re-entrancy
         require(!isExecuted[transactionHash], "Transaction has already been executed");
@@ -114,4 +126,30 @@ contract MinimumViableMultiSigWallet {
 
         require(success, "Transaction failed");
     }
+}
+
+/*
+owners
+0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c
+0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
+
+tx
+0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB
+1000000000000000000
+0x0
+0
+
+tx hash
+0x10f24745d5f82eeb1f41792592bad89fea5f42e05bc9ddaeb1c96e4f879420f2
+
+signatures
+0x042d82fc1ae7e693ed8d38acc3688efb06fa39b457c9c8e7ed88f95ef0c5f9c9084431e7fb971b0e508101f69ce8a543f0a4c9b51d8893e717ce8b3c6c30efb71b
+0xdb6b11f92f155fd13bd8a61081fc3a90d5d891e37d2bcf06e32c9c0bce6d9dee03e198870c8a68069168772af0276ea8a1cf397cad9a9d37e7741cf1706d17131b
+*/
+
+contract VerifySignature {
+  function getHash(bytes32 _hash) public pure returns (bytes32) {
+    // Here we are computing the hash of "Hello World", which has length 11.
+    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n11", _hash));
+  }
 }
