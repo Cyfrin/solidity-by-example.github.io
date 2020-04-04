efe4eaf269298ef5abadb5941f7fe20b3a4294a6
t4sk <tsk.nakamura@gmail.com>
2020-04-04 13:07:03 +0900

    create2

--- src/pages/Create2/index.html.js
+++ src/pages/Create2/index.html.js
@@ -1,8 +1,68 @@
 const html = `<p>Contract address can be precomputed, before the contract is deployed, using <code>create2</code></p>
 <pre><code class="language-solidity">pragma solidity ^0.5.16;
 
-// NOTE: use case (send ether to precomputed address, recover later)
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Create2.sol
+contract Factory {
+    event Deployed(address addr, uint256 salt);
+
+    // 1. Get bytecode of contract to be deployed
+    // NOTE: _owner and _foo are arguments of the TestContract&#39;s constructor
+    function getBytecode(address _owner, uint _foo) public pure returns (bytes memory) {
+        bytes memory bytecode = type(TestContract).creationCode;
+
+        return abi.encodePacked(bytecode, abi.encode(_owner, _foo));
+    }
+
+    // 2. Compute the address of the contract to be deployed
+    // NOTE: _salt is a random number used to create an address
+    function getAddress(bytes memory bytecode, uint _salt) public view returns (address) {
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                byte(0xff),
+                address(this),
+                _salt,
+                keccak256(bytecode)
+            )
+        );
+
+        // NOTE: cast last 20 bytes of hash to address
+        return address(uint160(uint256(hash)));
+    }
+
+    // 3. Deploy the contract
+    // NOTE:
+    // Check the event log Deployed which contains the address of the deployed TestContract.
+    // The address in the log should equal the address computed from above.
+    function deploy(bytes memory bytecode, uint _salt) public payable {
+        address addr;
+
+        /*
+        NOTE: How to call create2
+
+        create2(v, p, n, s)
+        create new contract with code at memory p to p + n
+        and send v wei
+        and return the new address
+        where new address = first 20 bytes of keccak256(0xff + address(this) + s + keccak256(mem[p…(p+n)))
+              s = big-endian 256-bit value
+        */
+        assembly {
+            addr := create2(
+                callvalue, // wei sent with current call
+                // Actual code starts after skipping the first 32 bytes
+                add(bytecode, 0x20),
+                mload(bytecode), // Load the size of code contained in the first 32 bytes
+                _salt // Salt from function arguments
+            )
+
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+
+        emit Deployed(addr, _salt);
+    }
+}
+
 contract TestContract {
     address public owner;
     uint public foo;
@@ -14,68 +74,6 @@ contract TestContract {
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
-    }
-}
-
-contract Factory {
-    event Deployed(address addr, uint256 salt);
-
-    function getBytecode(address _owner, uint _foo) public pure returns (bytes memory) {
-        bytes memory bytecode = type(TestContract).creationCode;
-
-        // TODO: learn difference between encodePacked and encode
-        // TODO: learn how contract bytecode is structured
-        return abi.encodePacked(bytecode, abi.encode(_owner, _foo));
-    }
-
-    function getAddress(bytes memory bytecode, uint _salt) public view returns (address) {
-        // TODO: learn difference between creation code and run time code
-        // NOTE: sha3(0xff ++ msg.sender ++ salt ++ sha3(init_code))[12:]
-        bytes32 hash = keccak256(
-            abi.encodePacked(
-                byte(0xff),
-                address(this),
-                _salt,
-                keccak256(bytecode)
-            )
-        );
-
-        // NOTE: cast last 20 bytes of hash to address
-        // TODO: learn how casting works
-        return address(uint160(uint256(hash)));
-    }
-
-    function deploy(bytes memory bytecode, uint _salt) public payable {
-        address addr;
-        // TODO: learn assembly, what&#39;s going on here?
-        // https://solidity.readthedocs.io/en/v0.5.15/assembly.html#
-        /*
-        create2(v, p, n, s)
-        create new contract with code mem[p…(p+n))
-        at address keccak256(0xff . this . s . keccak256(mem[p…(p+n)))
-        and send v wei
-        and return the new address,
-            where 0xff is a 1 byte value,
-            this is the current contract’s address as a 20 byte value
-            and s is a big-endian 256-bit value
-        */
-        assembly {
-            addr := create2(
-                callvalue, // wei sent with current call
-                // the code itself starts at the second slot.
-                // The first slot contains array length
-                add(bytecode, 0x20),
-                mload(bytecode), // size of code
-                _salt // salt from function arguments
-            )
-
-            // TODO: learn iszero, extcodesize, what&#39;s going on here?
-            if iszero(extcodesize(addr)) {
-                revert(0, 0)
-            }
-        }
-
-        emit Deployed(addr, _salt);
     }
 }
 </code></pre>
