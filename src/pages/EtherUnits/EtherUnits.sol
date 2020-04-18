f2b7fd3e0efcf0592aef8b036fc8b3bcc12cfbb2
t4sk <tsk.nakamura@gmail.com>
2020-04-18 09:43:53 +0900

    ether

--- src/pages/EtherUnits/EtherUnits.sol
+++ src/pages/EtherUnits/EtherUnits.sol
@@ -2,13 +2,10 @@ pragma solidity ^0.5.16;
 
 contract EtherUnits {
   uint public oneWei = 1 wei;
+  // 1 wei is equal to 1
+  bool public isOneWei = 1 wei == 1;
+
   uint public oneEther = 1 ether;
-
-  function testOneWei() public pure returns (bool) {
-    return 1 wei == 1;
-  }
-
-  function testOneEther() public pure returns (bool) {
-    return 1 ether == 1e18 wei;
-  }
+  // 1 ether is equal to 10^18
+  bool public isOneEther = 1 ether == 1e18;
 }
