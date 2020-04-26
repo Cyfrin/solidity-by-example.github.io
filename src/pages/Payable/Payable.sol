8e9cc8daa2229eb81f6a8e5af11e901d5adc2b63
t4sk <tsk.nakamura@gmail.com>
2020-04-27 05:24:08 +0900

    Payable

--- src/pages/Payable/Payable.sol
+++ src/pages/Payable/Payable.sol
@@ -1,44 +1,40 @@
 pragma solidity ^0.5.16;
 
-contract Wallet {
-    event Deposit(address sender, uint amount, uint balance);
-    event Withdraw(uint amount, uint balance);
-    event Transfer(address to, uint amount, uint balance);
-
+contract Payable {
     // Payable address can receive Ether
     address payable public owner;
 
+    // Payable constructor can receive Ether
     constructor() public payable {
         owner = msg.sender;
     }
 
-    // Try calling this function along with some ether.
+    // Function to deposit Ether into this contract.
+    // Call this function along with some Ether.
     // The balance of this contract will be automatically updated.
     function deposit() public payable {
-        emit Deposit(msg.sender, msg.value, address(this).balance);
     }
 
-    // Try calling this function along with some ether.
-    // The function would throw an error since this function is not payable.
+    // Call this function along with some Ether.
+    // The function will throw an error since this function is not payable.
     function notPayable() public {
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
-        _;
+    // Function to withdraw all Ether from this contract.
+    function withdraw() public {
+        // get the amount of Ether stored in this contract
+        uint amount = address(this).balance;
+
+        // send all Ether to owner
+        // Owner can receive Ether since the address of owner is payable
+        (bool success,) = owner.call.value(amount)("");
+        require(success, "Failed to send Ether");
     }
 
-    function withdraw(uint _amount) public onlyOwner {
-        owner.transfer(_amount);
-        emit Withdraw(_amount, address(this).balance);
-    }
-
-    function transfer(address payable _to, uint _amount) public onlyOwner {
-        _to.transfer(_amount);
-        emit Transfer(_to, _amount, address(this).balance);
-    }
-
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
+    // Function to transfer Ether from this contract to address from input
+    function transfer(address payable _to, uint _amount) public {
+        // Note that "to" is declared as payable
+        (bool success,) = _to.call.value(_amount)("");
+        require(success, "Failed to send Ether");
     }
 }
