b6e132c643d860bd795f189be726c92d992bee4b
t4sk <tsk.nakamura@gmail.com>
2020-05-07 07:13:04 +0900

    payment channels

--- memo/BiDirectionalPaymentChannel.sol
+++ memo/BiDirectionalPaymentChannel.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.11;
+pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
 /*
@@ -32,22 +32,22 @@ import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contr
 
 
 contract BiDirectionalPaymentChannel {
-    using SafeMath for uint256;
+    using SafeMath for uint;
     using ECDSA for bytes32;
 
-    event ChallengeExit(address indexed sender, uint256 nonce);
-    event Withdraw(address indexed to, uint256 amount);
+    event ChallengeExit(address indexed sender, uint nonce);
+    event Withdraw(address indexed to, uint amount);
 
     address payable[2] public users;
     mapping(address => bool) public isUser;
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint) public balances;
 
-    uint256 public challengePeriod;
-    uint256 public expiresAt;
-    uint256 public nonce;
+    uint public challengePeriod;
+    uint public expiresAt;
+    uint public nonce;
 
-    modifier checkBalances(uint256[2] memory _balances) {
+    modifier checkBalances(uint[2] memory _balances) {
         require(
             address(this).balance >= _balances[0].add(_balances[1]),
             "balance of contract must be >= to the total balance of users"
@@ -58,14 +58,14 @@ contract BiDirectionalPaymentChannel {
     // NOTE: deposit from multi-sig wallet
     constructor(
         address payable[2] memory _users,
-        uint256[2] memory _balances,
-        uint256 _expiresAt,
-        uint256 _challengePeriod
+        uint[2] memory _balances,
+        uint _expiresAt,
+        uint _challengePeriod
     ) public payable checkBalances(_balances) {
         require(_expiresAt > block.timestamp, "Expiration must be > now");
         require(_challengePeriod > 0, "Challenge period must be > 0");
 
-        for (uint256 i = 0; i < _users.length; i++) {
+        for (uint i = 0; i < _users.length; i++) {
             address payable user = _users[i];
 
             require(!isUser[user], "user must be unique");
@@ -83,10 +83,10 @@ contract BiDirectionalPaymentChannel {
         bytes[2] memory _signatures,
         address _contract,
         address[2] memory _signers,
-        uint256[2] memory _balances,
-        uint256 _nonce
+        uint[2] memory _balances,
+        uint _nonce
     ) public pure returns (bool) {
-        for (uint256 i = 0; i < _signatures.length; i++) {
+        for (uint i = 0; i < _signatures.length; i++) {
             /*
             NOTE: sign with address of this contract to protect
                   agains replay attack on other contracts
@@ -106,12 +106,12 @@ contract BiDirectionalPaymentChannel {
 
     modifier checkSignatures(
         bytes[2] memory _signatures,
-        uint256[2] memory _balances,
-        uint256 _nonce
+        uint[2] memory _balances,
+        uint _nonce
     ) {
         // NOTE: need to cast payable address to address type (not in 0.6)
         address[2] memory signers;
-        for (uint256 i = 0; i < users.length; i++) {
+        for (uint i = 0; i < users.length; i++) {
             signers[i] = address(users[i]);
         }
 
@@ -129,8 +129,8 @@ contract BiDirectionalPaymentChannel {
     }
 
     function challengeExit(
-        uint256[2] memory _balances,
-        uint256 _nonce,
+        uint[2] memory _balances,
+        uint _nonce,
         bytes[2] memory _signatures
     )
         public
@@ -141,7 +141,7 @@ contract BiDirectionalPaymentChannel {
         require(block.timestamp < expiresAt, "Expired challenge period");
         require(_nonce > nonce, "Nonce must be greater than the current nonce");
 
-        for (uint256 i = 0; i < _balances.length; i++) {
+        for (uint i = 0; i < _balances.length; i++) {
             balances[users[i]] = _balances[i];
         }
 
@@ -157,7 +157,7 @@ contract BiDirectionalPaymentChannel {
             "Challenge period has not expired yet"
         );
 
-        uint256 amount = balances[msg.sender];
+        uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
 
         (bool sent, ) = msg.sender.call.value(amount)("");
--- memo/UniDirectionalPaymentChannel.sol
+++ memo/UniDirectionalPaymentChannel.sol
@@ -6,15 +6,15 @@ import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contr
 
 
 contract UniDirectionalPaymentChannel is ReentrancyGuard {
-    using SafeMath for uint256;
+    using SafeMath for uint;
     using ECDSA for bytes32;
 
     address payable public payer;
     address payable public payee;
 
-    uint256 expiresAt;
+    uint public expiresAt;
 
-    constructor(address payable _payee, uint256 _expiresAt) public payable {
+    constructor(address payable _payee, uint _expiresAt) public payable {
         require(_expiresAt > block.timestamp, "Expiration must be > now");
 
         payer = msg.sender;
@@ -27,7 +27,7 @@ contract UniDirectionalPaymentChannel is ReentrancyGuard {
         bytes memory _signature,
         address _payer,
         address _contract,
-        uint256 _payeeBalance
+        uint _payeeBalance
     ) public pure returns (bool) {
         // NOTE: sign with address of this contract to protect agains
         // replay attack on other contracts
@@ -37,7 +37,7 @@ contract UniDirectionalPaymentChannel is ReentrancyGuard {
                 .recover(_signature) == _payer;
     }
 
-    modifier checkSignature(bytes memory _signature, uint256 _payeeBalance) {
+    modifier checkSignature(bytes memory _signature, uint _payeeBalance) {
         require(
             verify(_signature, payer, address(this), _payeeBalance),
             "Invalid signature"
@@ -45,8 +45,7 @@ contract UniDirectionalPaymentChannel is ReentrancyGuard {
         _;
     }
 
-    // TODO re-entrancy guard
-    function close(uint256 _payeeBalance, bytes memory _signature)
+    function close(uint _payeeBalance, bytes memory _signature)
         public
         nonReentrant
         checkSignature(_signature, _payeeBalance)
