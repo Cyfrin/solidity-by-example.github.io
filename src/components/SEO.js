eb73ebdd380436ecd51d891b853eaf9cdb91196c
t4sk <tsk.nakamura@gmail.com>
2020-05-19 07:05:08 +0900

    update npm

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,6 @@
-import React from "react"
 import PropTypes from "prop-types"
-import Helmet from "react-helmet"
+import React from "react"
+import { Helmet } from "react-helmet"
 
 function SEO({ title, description }) {
   return (
