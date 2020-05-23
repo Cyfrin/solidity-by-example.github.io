0ced0bf72d9aa4b7779c6b2c769825fa7478eb51
t4sk <tsk.nakamura@gmail.com>
2020-05-24 06:33:47 +0900

    move files

---
+++ src/pages/app/Create2/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Example from "../../../components/Example"
+import html from "./index.html.js"
+
+export default function (props) {
+  return (
+    <Example
+      title="Precompute Contract Address with Create2"
+      description="Precompute contract address with create2"
+      html={html}
+    />
+  )
+}
---
+++ src/pages/hacks/BlockTimestampManipulation/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Example from "../../../components/Example"
+import html from "./index.html.js"
+
+export default function (props) {
+  return (
+    <Example
+      title="Block Timestamp Manipulation"
+      description="An example of a Solidity contract vulnerable to block timestamp manipulation"
+      html={html}
+    />
+  )
+}
