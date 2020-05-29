4b59da00e274e1369d2bb46c52c913b28bd75510
t4sk <tsk.nakamura@gmail.com>
2020-05-29 18:01:59 +0900

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import styles from "./App.module.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Home from "./pages/0.5/Home"
-import routes from "./pages/0.5/routes"
+import Home_ver_0_5 from "./pages/0.5/Home"
+import routes_ver_0_5 from "./pages/0.5/routes"
 
 function App(props) {
   return (
@@ -13,14 +13,14 @@ function App(props) {
         <Header />
         <div className={styles.main}>
           <Switch>
-            {routes.map((route) => (
+            {routes_ver_0_5.map((route) => (
               <Route
                 key={route.path}
                 path={route.path}
                 component={route.component}
               />
             ))}
-            <Route component={Home} />
+            <Route component={Home_ver_0_5} />
           </Switch>
         </div>
         <div className={styles.footer}>
