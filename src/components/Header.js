c3ce9f8da9cdc70280ef09cf231cb004cd154955
t4sk <tsk.nakamura@gmail.com>
2020-05-29 16:36:51 +0900

    header select version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,37 @@
 import React from "react"
+import { useHistory, useParams } from "react-router-dom"
 import styles from "./Header.module.css"
 import logo from "./logo.svg"
-import { SOL_VERSION } from "../constants"
+import { SOL_VERSIONS } from "../constants"
 
 function Header(props) {
+  const { version = SOL_VERSIONS[0] } = useParams()
+  const history = useHistory()
+
+  function onSelect(e) {
+    const version = e.target.value
+    history.push(`/${version}`)
+  }
+
   return (
     <div className={styles.component}>
       <a href="/">
         <img src={logo} alt="logo" className={styles.logo} />
       </a>
 
-      <h3>
-        <a href="/">
-          <div>Solidity by Example</div>
-          <div>version {SOL_VERSION}</div>
-        </a>
+      <h3 className={styles.header}>
+        <a href="/">Solidity by Example</a>
+
+        <div className={styles.select}>
+          <div className={styles.version}>version</div>
+          <select value={version} onChange={onSelect}>
+            {SOL_VERSIONS.map((ver) => (
+              <option key={ver} value={ver}>
+                {ver}
+              </option>
+            ))}
+          </select>
+        </div>
       </h3>
     </div>
   )
