c4b02973b00afce90649ae83fcf5e1ddd64a68b1
t4sk <tsk.nakamura@gmail.com>
2020-05-29 12:20:40 +0900

    0.6 super

--- src/pages/Super/Super.sol
+++ src/pages/Super/Super.sol
@@ -14,33 +14,33 @@ contract A {
     // In our case, this will be useful for tracing function calls.
     event Log(string message);
 
-    function foo() public {
+    function foo() public virtual {
         emit Log("A.foo called");
     }
 
-    function bar() public {
+    function bar() public virtual {
         emit Log("A.bar called");
     }
 }
 
 contract B is A {
-    function foo() public {
+    function foo() public virtual override {
         emit Log("B.foo called");
         A.foo();
     }
-    function bar() public {
+    function bar() public virtual override {
         emit Log("B.bar called");
         super.bar();
     }
 }
 
 contract C is A {
-    function foo() public {
+    function foo() public virtual override {
         emit Log("C.foo called");
         A.foo();
     }
 
-    function bar() public {
+    function bar() public virtual override {
         emit Log("C.bar called");
         super.bar();
     }
@@ -53,4 +53,12 @@ contract D is B, C {
     // - Call D.bar and check the transaction logs
     //   D called C, then B, and finally A.
     //   Although super was called twice (by B and C) it only called A once.
+
+    function foo() public override(B, C) {
+        super.foo();
+    }
+
+    function bar() public override(B, C) {
+        super.bar();
+    }
 }
