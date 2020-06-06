dd696837327666da65e2bc556d158ca5dd667908
t4sk <tsk.nakamura@gmail.com>
2020-06-06 20:24:51 +0900

    md to react compute path to Example component

--- scripts/md-to-react.ts
+++ scripts/md-to-react.ts
@@ -1,8 +1,23 @@
 import fs from "fs"
 import path from "path"
+import mustache from "mustache"
 import { copy, getExtension } from "./lib"
 import mdToHtml from "./md-to-html"
-const { readdir } = fs.promises
+
+const { readdir, readFile, writeFile } = fs.promises
+
+function getImportExamplePath(dir: string): string {
+  const folders = dir.split("/")
+
+  const start = folders.findIndex((folder) => folder === "src")
+  if (start === -1) {
+    throw new Error(`Cannot find src`)
+  }
+
+  const depth = folders.length - (start + 1)
+
+  return path.join("../".repeat(depth), "components/Example")
+}
 
 async function main() {
   const args = process.argv.slice(2)
@@ -14,10 +29,16 @@ async function main() {
 
   for (const file of files) {
     await mdToHtml(path.join(dir, file))
-    await copy(
-      path.join(__dirname, "./template/index.js.mustache"),
-      path.join(dir, "index.js")
-    )
+
+    // create index.js
+    const jsTemplate = (
+      await readFile(path.join(__dirname, "./template/index.js.mustache"))
+    ).toString()
+    const js = mustache.render(jsTemplate, {
+      importExamplePath: getImportExamplePath(dir),
+    })
+    await writeFile(path.join(dir, `index.js`), js)
+
     await copy(
       path.join(__dirname, "./template/index.test.js"),
       path.join(dir, "index.test.js")
