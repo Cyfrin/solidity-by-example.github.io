2fccbed29677ca30bd9024db68ee53f0a63aa750
t4sk <tsk.nakamura@gmail.com>
2020-06-15 07:54:30 +0900

    fix CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,6 @@ import React from "react"
 import { shallow } from "enzyme"
 import App from "./App"
 
-test("CI", () => {
-  expect(1).toEqual(2)
-})
-
 test("it renders", () => {
   const component = shallow(<App />)
 
