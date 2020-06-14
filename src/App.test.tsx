b955d889af72c804b0c47a8b859fedc1869a30ee
t4sk <tsk.nakamura@gmail.com>
2020-06-15 07:46:28 +0900

    test CI fails

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import { shallow } from "enzyme"
 import App from "./App"
 
+test("CI", () => {
+  expect(1).toEqual(2)
+})
+
 test("it renders", () => {
   const component = shallow(<App />)
 
