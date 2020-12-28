0525e8140ac066b5ec94771ca69d8ee71b9f7c13
t4sk <tsk.nakamura@gmail.com>
2020-12-28 19:31:20 +0900

    delete 0.5

--- src/pages/0.5/hello-world/HelloWorld.sol
+++
@@ -1,6 +0,0 @@
-// compiler version must be greater than or equal to 0.5.16 and less than 0.6.0
-pragma solidity ^0.5.16;
-
-contract HelloWorld {
-    string public greet = "Hello World!";
-}
--- src/pages/0.5/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { shallow } from "enzyme"
-import Home from "./index"
-
-test("it renders", () => {
-  const component = shallow(<Home />)
-
-  expect(component).toMatchSnapshot()
-})
