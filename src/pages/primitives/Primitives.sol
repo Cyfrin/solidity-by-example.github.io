c9dd8b99afd70f4116e5c1d18d2f3eee9ec12bd6
t4sk <tsk.nakamura@gmail.com>
2021-10-04 19:02:51 +0900

    primitive

--- src/pages/primitives/Primitives.sol
+++ src/pages/primitives/Primitives.sol
@@ -13,16 +13,23 @@ contract Primitives {
         uint256 ranges from 0 to 2 ** 256 - 1
     */
     uint8 public u8 = 1;
-    uint256 public u256 = 456;
+    uint public u256 = 456;
     uint public u = 123; // uint is an alias for uint256
 
     /*
     Negative numbers are allowed for int types.
     Like uint, different ranges are available from int8 to int256
+    
+    int256 ranges from -2 ** 255 to 2 ** 255 - 1
+    int128 ranges from -2 ** 127 to 2 ** 127 - 1
     */
     int8 public i8 = -1;
-    int256 public i256 = 456;
+    int public i256 = 456;
     int public i = -123; // int is same as int256
+
+    // minimum and maximum of int
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
 
     address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
 
