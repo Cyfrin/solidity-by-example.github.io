c0dc278c00c19844631351f8e20bad492c0f18da
wiasliaw <leewei48750@gmail.com>
2021-10-07 16:18:26 +0800

    feat(new-contract): `new` with `create2` options

--- src/pages/new-contract/NewContract.sol
+++ src/pages/new-contract/NewContract.sol
@@ -26,6 +26,16 @@ contract CarFactory {
         cars.push(car);
     }
 
+    function create2(address _owner, string memory _model, bytes32 _salt) public {
+        Car car = (new Car){salt: _salt}(_owner, _model);
+        cars.push(car);
+    }
+
+    function create2AndSendEther(address _owner, string memory _model, bytes32 _salt) public payable {
+        Car car = (new Car){value: msg.value, salt: _salt}(_owner, _model);
+        cars.push(car);
+    }
+
     function getCar(uint _index)
         public
         view
--- src/pages/new-contract/index.md
+++ src/pages/new-contract/index.md
@@ -4,7 +4,8 @@ version: 0.8.3
 description: Learn how to create new contracts from inside of a contract with Solidity
 ---
 
-Contracts can be created by other contracts using the `new` keyword.
+Contracts can be created by other contracts using the `new` keyword. Since 0.8.0, `new` keyword supports `create2` feature by specifying `salt` options.
+
 
 ```solidity
 {{{NewContract}}}
