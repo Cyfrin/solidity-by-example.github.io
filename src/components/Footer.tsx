e77619c46002d14a435f327e03db71375225c92b
t4sk <tsk.nakamura@gmail.com>
2021-10-19 18:15:35 +0900

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import styles from "./Footer.module.css"
 import youTube from "./youtube.svg"
+import telegram from "./telegram.png"
+import discord from "./discord.png"
 
 // Khan Academy
 const ADDRESS = "0x95a647B3d8a3F11176BAdB799b9499C671fa243a"
@@ -8,24 +10,28 @@ const ADDRESS = "0x95a647B3d8a3F11176BAdB799b9499C671fa243a"
 function Footer() {
   return (
     <div className={styles.component}>
-      <div className={styles.row}>
-        <a href="https://discord.gg/bt8FK2wkcW" target="__blank">
-          Discord
-        </a>
-      </div>
-      <div className={styles.row}>
-        <a href="https://t.me/smartcontractprogrammer" target="__blank">
-          Telegram
-        </a>
-      </div>
-      <div className={styles.row}>
-        <img src={youTube} alt="logo" className={styles.youTube} />
-        <a
-          href="https://www.youtube.com/channel/UCJWh7F3AFyQ_x01VKzr9eyA"
-          target="__blank"
-        >
-          Smart Contract Programmer
-        </a>
+      <div className={styles.social}>
+        <div className={styles.row}>
+          <img src={discord} alt="logo" className={styles.discord} />
+          <a href="https://discord.gg/bt8FK2wkcW" target="__blank">
+            Discord
+          </a>
+        </div>
+        <div className={styles.row}>
+          <img src={telegram} alt="logo" className={styles.telegram} />
+          <a href="https://t.me/smartcontractprogrammer" target="__blank">
+            Telegram
+          </a>
+        </div>
+        <div className={styles.row}>
+          <img src={youTube} alt="logo" className={styles.youTube} />
+          <a
+            href="https://www.youtube.com/channel/UCJWh7F3AFyQ_x01VKzr9eyA"
+            target="__blank"
+          >
+            YouTube
+          </a>
+        </div>
       </div>
       <div className={styles.row}>
         Donate
