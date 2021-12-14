d5787ffd0f59b74284180d9071b2cbc0ff2483fb
Andriamalala Tantely <andriatanteli@gmail.com>
2021-12-14 23:11:03 +0100

    added byte type

--- src/pages/primitives/Primitives.sol
+++ src/pages/primitives/Primitives.sol
@@ -33,6 +33,19 @@ contract Primitives {
 
     address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
 
+    /*
+    In Solidity, the data type byte represent a sequence of bytes. 
+    Solidity presents two type of bytes types :
+
+     - fixed-sized byte arrays
+     - dynamically-sized byte arrays.
+     
+     The term bytes in Solidity represents a dynamic array of bytes. 
+     It’s a shorthand for byte[] .
+    */
+    bytes1 a = 0xb5; //  [10110101]
+    bytes1 b = 0x56; //  [01010110]
+
     // Default values
     // Unassigned variables have a default value
     bool public defaultBoo; // false
