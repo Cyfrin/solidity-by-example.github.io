9f515c4e1603092f2b2e5c8f0b5397ecc2e2d176
t4sk <tsk.nakamura@gmail.com>
2022-02-14 18:40:21 +0900

    erc20 example

--- src/pages/app/erc20/ERC20.sol
+++ src/pages/app/erc20/ERC20.sol
@@ -7,8 +7,8 @@ contract ERC20 is IERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "SOL";
-    string public symbol = "Solidity by Example";
+    string public name = "Solidity by Example";
+    string public symbol = "SOLBYEX";
     uint8 public decimals = 18;
 
     function transfer(address recipient, uint amount) external returns (bool) {
