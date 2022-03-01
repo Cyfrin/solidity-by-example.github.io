27bfa8661a18bcbe67ca07a92c70b361529cfab4
t4sk <tsk.nakamura@gmail.com>
2022-03-01 18:13:19 +0900

    update multi-delegatecall

--- src/pages/app/multi-delegatecall/MultiDelegatecall.sol
+++ src/pages/app/multi-delegatecall/MultiDelegatecall.sol
@@ -21,15 +21,42 @@ contract MultiDelegatecall {
     }
 }
 
+// Why use multi delegatecall? Why not multi call?
+// alice -> multi call --- call ---> test (msg.sender = multi call)
+// alice -> test --- delegatecall ---> test (msg.sender = alice)
 contract TestMultiDelegatecall is MultiDelegatecall {
     event Log(address caller, string func, uint i);
 
     function func1(uint x, uint y) external {
+        // msg.sender = alice
         emit Log(msg.sender, "func1", x + y);
     }
 
     function func2() external returns (uint) {
+        // msg.sender = alice
         emit Log(msg.sender, "func2", 2);
         return 111;
+    }
+
+    mapping(address => uint) public balanceOf;
+
+    // WARNING: unsafe code when used in combination with multi-delegatecall
+    // user can mint multiple times for the price of msg.value
+    function mint() external payable {
+        balanceOf[msg.sender] += msg.value;
+    }
+}
+
+contract Helper {
+    function getFunc1Data(uint x, uint y) external pure returns (bytes memory) {
+        return abi.encodeWithSelector(TestMultiDelegatecall.func1.selector, x, y);
+    }
+
+    function getFunc2Data() external pure returns (bytes memory) {
+        return abi.encodeWithSelector(TestMultiDelegatecall.func2.selector);
+    }
+
+    function getMintData() external pure returns (bytes memory) {
+        return abi.encodeWithSelector(TestMultiDelegatecall.mint.selector);
     }
 }
--- src/pages/app/multi-delegatecall/index.html.ts
+++ src/pages/app/multi-delegatecall/index.html.ts
@@ -1,8 +1,7 @@
 // metadata
 export const version = "0.8.10"
 export const title = "Multi Delegatecall"
-export const description =
-  "An example of contract to call multiple functions in a single transaction"
+export const description = "An example of contract to call multiple functions in a single transaction"
 
 const html = `<p>An example of calling multiple functions with a single transaction, using <code>delegatecall</code>.</p>
 <pre><code class="language-solidity"><span class="hljs-comment">// SPDX-License-Identifier: MIT</span>
@@ -28,16 +27,43 @@ const html = `<p>An example of calling multiple functions with a single transact
     }
 }
 
+<span class="hljs-comment">// Why use multi delegatecall? Why not multi call?</span>
+<span class="hljs-comment">// alice -&gt; multi call --- call ---&gt; test (msg.sender = multi call)</span>
+<span class="hljs-comment">// alice -&gt; test --- delegatecall ---&gt; test (msg.sender = alice)</span>
 <span class="hljs-class"><span class="hljs-keyword">contract</span> <span class="hljs-title">TestMultiDelegatecall</span> <span class="hljs-keyword">is</span> <span class="hljs-title">MultiDelegatecall</span> </span>{
     <span class="hljs-function"><span class="hljs-keyword">event</span> <span class="hljs-title">Log</span>(<span class="hljs-params"><span class="hljs-keyword">address</span> caller, <span class="hljs-keyword">string</span> func, <span class="hljs-keyword">uint</span> i</span>)</span>;
 
     <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">func1</span>(<span class="hljs-params"><span class="hljs-keyword">uint</span> x, <span class="hljs-keyword">uint</span> y</span>) <span class="hljs-title"><span class="hljs-keyword">external</span></span> </span>{
+        <span class="hljs-comment">// msg.sender = alice</span>
         <span class="hljs-keyword">emit</span> Log(<span class="hljs-built_in">msg</span>.<span class="hljs-built_in">sender</span>, <span class="hljs-string">"func1"</span>, x <span class="hljs-operator">+</span> y);
     }
 
     <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">func2</span>(<span class="hljs-params"></span>) <span class="hljs-title"><span class="hljs-keyword">external</span></span> <span class="hljs-title"><span class="hljs-keyword">returns</span></span> (<span class="hljs-params"><span class="hljs-keyword">uint</span></span>) </span>{
+        <span class="hljs-comment">// msg.sender = alice</span>
         <span class="hljs-keyword">emit</span> Log(<span class="hljs-built_in">msg</span>.<span class="hljs-built_in">sender</span>, <span class="hljs-string">"func2"</span>, <span class="hljs-number">2</span>);
         <span class="hljs-keyword">return</span> <span class="hljs-number">111</span>;
+    }
+
+    <span class="hljs-keyword">mapping</span>(<span class="hljs-keyword">address</span> <span class="hljs-operator">=</span><span class="hljs-operator">&gt;</span> <span class="hljs-keyword">uint</span>) <span class="hljs-keyword">public</span> balanceOf;
+
+    <span class="hljs-comment">// WARNING: unsafe code when used in combination with multi-delegatecall</span>
+    <span class="hljs-comment">// user can mint multiple times for the price of msg.value</span>
+    <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">mint</span>(<span class="hljs-params"></span>) <span class="hljs-title"><span class="hljs-keyword">external</span></span> <span class="hljs-title"><span class="hljs-keyword">payable</span></span> </span>{
+        balanceOf[<span class="hljs-built_in">msg</span>.<span class="hljs-built_in">sender</span>] <span class="hljs-operator">+</span><span class="hljs-operator">=</span> <span class="hljs-built_in">msg</span>.<span class="hljs-built_in">value</span>;
+    }
+}
+
+<span class="hljs-class"><span class="hljs-keyword">contract</span> <span class="hljs-title">Helper</span> </span>{
+    <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">getFunc1Data</span>(<span class="hljs-params"><span class="hljs-keyword">uint</span> x, <span class="hljs-keyword">uint</span> y</span>) <span class="hljs-title"><span class="hljs-keyword">external</span></span> <span class="hljs-title"><span class="hljs-keyword">pure</span></span> <span class="hljs-title"><span class="hljs-keyword">returns</span></span> (<span class="hljs-params"><span class="hljs-keyword">bytes</span> <span class="hljs-keyword">memory</span></span>) </span>{
+        <span class="hljs-keyword">return</span> <span class="hljs-built_in">abi</span>.<span class="hljs-built_in">encodeWithSelector</span>(TestMultiDelegatecall.func1.<span class="hljs-built_in">selector</span>, x, y);
+    }
+
+    <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">getFunc2Data</span>(<span class="hljs-params"></span>) <span class="hljs-title"><span class="hljs-keyword">external</span></span> <span class="hljs-title"><span class="hljs-keyword">pure</span></span> <span class="hljs-title"><span class="hljs-keyword">returns</span></span> (<span class="hljs-params"><span class="hljs-keyword">bytes</span> <span class="hljs-keyword">memory</span></span>) </span>{
+        <span class="hljs-keyword">return</span> <span class="hljs-built_in">abi</span>.<span class="hljs-built_in">encodeWithSelector</span>(TestMultiDelegatecall.func2.<span class="hljs-built_in">selector</span>);
+    }
+
+    <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">getMintData</span>(<span class="hljs-params"></span>) <span class="hljs-title"><span class="hljs-keyword">external</span></span> <span class="hljs-title"><span class="hljs-keyword">pure</span></span> <span class="hljs-title"><span class="hljs-keyword">returns</span></span> (<span class="hljs-params"><span class="hljs-keyword">bytes</span> <span class="hljs-keyword">memory</span></span>) </span>{
+        <span class="hljs-keyword">return</span> <span class="hljs-built_in">abi</span>.<span class="hljs-built_in">encodeWithSelector</span>(TestMultiDelegatecall.mint.<span class="hljs-built_in">selector</span>);
     }
 }
 </code></pre>
