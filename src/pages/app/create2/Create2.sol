4a53d0f2b8cf5b14b4ad3a4f1e8e341a527d4143
Devan Non <devanon@chainstride.capital>
2022-03-10 18:12:36 +0000

    use newer create2 syntax

--- src/pages/app/create2/Create2.sol
+++ src/pages/app/create2/Create2.sol
@@ -2,6 +2,20 @@
 pragma solidity ^0.8.10;
 
 contract Factory {
+    // Returns the address of the newly deployed contract
+    function deploy(
+        address _owner,
+        uint _foo,
+        bytes32 _salt
+    ) public payable returns (address) {
+        // This syntax is a newer way to invoke create2 without assembly, you just need to pass salt
+        // https://docs.soliditylang.org/en/latest/control-structures.html#salted-contract-creations-create2
+        return address(new TestContract{salt: _salt}(_owner, _foo));
+    }
+}
+
+// This is the older way of doing it using assembly
+contract FactoryAssembly {
     event Deployed(address addr, uint salt);
 
     // 1. Get bytecode of contract to be deployed
