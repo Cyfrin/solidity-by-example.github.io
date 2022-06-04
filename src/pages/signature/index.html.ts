f57656fd4eec8e0aac8b7ca52cef25e6a33ee78b
t4sk <tsk.nakamura@gmail.com>
2022-06-04 10:15:38 +0900

    signature

--- src/pages/signature/index.html.ts
+++ src/pages/signature/index.html.ts
@@ -4,6 +4,7 @@ export const title = "Verifying Signature"
 export const description = "An example of how to verify signatures in Solidity"
 
 const html = `<p>Messages can be signed off chain and then verified on chain using a smart contract.</p>
+<p><a href="https://github.com/t4sk/hello-erc20-permit/blob/main/test/verify-signature.js">Example using ethers.js</a></p>
 <pre><code class="language-solidity"><span class="hljs-comment">// SPDX-License-Identifier: MIT</span>
 <span class="hljs-meta"><span class="hljs-keyword">pragma</span> <span class="hljs-keyword">solidity</span> ^0.8.13;</span>
 
--- src/pages/signature/index.md
+++ src/pages/signature/index.md
@@ -6,6 +6,8 @@ description: An example of how to verify signatures in Solidity
 
 Messages can be signed off chain and then verified on chain using a smart contract.
 
+[Example using ethers.js](https://github.com/t4sk/hello-erc20-permit/blob/main/test/verify-signature.js)
+
 ```solidity
 {{{Signature}}}
 ```
