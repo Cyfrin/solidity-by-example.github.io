52c37f60f74bd401e192a7bc6907e94f42b051e5
0xlead <94481845+0xlead@users.noreply.github.com>
2022-12-27 22:02:32 +0300

    Create CallingContractWithInterface.sol

---
+++ src/pages/calling-contract/CallingContractWithInterface.sol
@@ -0,0 +1,47 @@
+// File: Callee.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Callee {
+    uint public x;
+    uint public value;
+
+    function setX(uint _x) public returns (uint) {
+        x = _x;
+        return x;
+    }
+
+    function setXandSendEther(uint _x) public payable returns (uint, uint) {
+        x = _x;
+        value = msg.value;
+
+        return (x, value);
+    }
+}
+
+
+// File: Caller.sol
+//! When defining an interface, the I prefix refers to it as Interface.
+interface ICallee {
+  function setX(uint _x) external returns (uint);
+  function setXandSendEther(uint _x) external payable returns (uint, uint);
+}
+
+
+contract Caller {
+   ICallee calleContract;
+   
+   constructor(address _calleAddress) {
+    calleContract = ICallee(_calleAddress);
+   }
+   
+    function setXFromAddress(uint _x) public {
+        calleContract.setX(_x);
+    }
+
+    function setXandSendEther(uint _x) public payable {
+        (uint x, uint value) = calleContract.setXandSendEther{value: msg.value}(_x);
+    }
+    
+    
+}
