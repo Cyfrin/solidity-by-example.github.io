2ee11434d174ae44e7f263589a969fcbf544c48c
t4sk <tsk.nakamura@gmail.com>
2023-09-22 18:52:14 +0900

    yul variable

--- src/pages/assembly-variable/AssemblyVariable.sol
+++ src/pages/assembly-variable/AssemblyVariable.sol
@@ -5,7 +5,6 @@ contract AssemblyVariable {
     function yul_let() public pure returns (uint z) {
         assembly {
             // Language used for assembly is called Yul
-            
             // Local variables
             let x := 123
             z := 456
--- src/pages/assembly-variable/index.html.ts
+++ src/pages/assembly-variable/index.html.ts
@@ -8,7 +8,7 @@ export const keywords = ["assembly", "variable", "yul"]
 export const codes = [
     {
         fileName: "AssemblyVariable.sol",
-        code: "Ly8gU1BEWC1MaWNlbnNlLUlkZW50aWZpZXI6IE1JVApwcmFnbWEgc29saWRpdHkgXjAuOC4yMDsKCmNvbnRyYWN0IEFzc2VtYmx5VmFyaWFibGUgewogICAgZnVuY3Rpb24geXVsX2xldCgpIHB1YmxpYyBwdXJlIHJldHVybnMgKHVpbnQgeikgewogICAgICAgIGFzc2VtYmx5IHsKICAgICAgICAgICAgLy8gTGFuZ3VhZ2UgdXNlZCBmb3IgYXNzZW1ibHkgaXMgY2FsbGVkIFl1bAogICAgICAgICAgICAKICAgICAgICAgICAgLy8gTG9jYWwgdmFyaWFibGVzCiAgICAgICAgICAgIGxldCB4IDo9IDEyMwogICAgICAgICAgICB6IDo9IDQ1NgogICAgICAgIH0KICAgIH0KfQo=",
+        code: "Ly8gU1BEWC1MaWNlbnNlLUlkZW50aWZpZXI6IE1JVApwcmFnbWEgc29saWRpdHkgXjAuOC4yMDsKCmNvbnRyYWN0IEFzc2VtYmx5VmFyaWFibGUgewogICAgZnVuY3Rpb24geXVsX2xldCgpIHB1YmxpYyBwdXJlIHJldHVybnMgKHVpbnQgeikgewogICAgICAgIGFzc2VtYmx5IHsKICAgICAgICAgICAgLy8gTGFuZ3VhZ2UgdXNlZCBmb3IgYXNzZW1ibHkgaXMgY2FsbGVkIFl1bAogICAgICAgICAgICAvLyBMb2NhbCB2YXJpYWJsZXMKICAgICAgICAgICAgbGV0IHggOj0gMTIzCiAgICAgICAgICAgIHogOj0gNDU2CiAgICAgICAgfQogICAgfQp9Cg==",
     },
 ]
 
@@ -20,7 +20,6 @@ const html = `<p>Example of how to declare variables inside <code>assembly</code
     <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">yul_let</span>(<span class="hljs-params"></span>) <span class="hljs-title"><span class="hljs-keyword">public</span></span> <span class="hljs-title"><span class="hljs-keyword">pure</span></span> <span class="hljs-title"><span class="hljs-keyword">returns</span></span> (<span class="hljs-params"><span class="hljs-keyword">uint</span> z</span>) </span>{
         <span class="hljs-keyword">assembly</span> {
             <span class="hljs-comment">// Language used for assembly is called Yul</span>
-            
             <span class="hljs-comment">// Local variables</span>
             <span class="hljs-keyword">let</span> x <span class="hljs-operator">:=</span> <span class="hljs-number">123</span>
             z <span class="hljs-operator">:=</span> <span class="hljs-number">456</span>
