98af3e24ecedde7502a8f40817d7e2d91a2c5f90
open dev <opendev.top@gmail.com>
2024-05-07 16:21:16 +0800

    Update EtherUnits.sol

--- contracts/src/ether-units/EtherUnits.sol
+++ contracts/src/ether-units/EtherUnits.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.24;
 
 contract EtherUnits {
-    uint256 public oneWei = 1 wei;
-    // 1 wei is equal to 1
-    bool public isOneWei = 1 wei == 1;
+	uint256 public oneWei = 1 wei;
+	// 1 wei is equal to 1
+	bool public isOneWei = (oneWei == 1);
 
-    uint256 public oneEther = 1 ether;
-    // 1 ether is equal to 10^18 wei
-    bool public isOneEther = 1 ether == 1e18;
+	uint256 public oneGwei = 1 gwei;
+	// 1 gwei is equal to 10^9 gwei
+	bool public isOneGwei = (oneGwei == 1e9);
+
+	uint256 public oneEther = 1 ether;
+	// 1 ether is equal to 10^18 wei
+	bool public isOneEther = (oneEther == 1e18);
 }
