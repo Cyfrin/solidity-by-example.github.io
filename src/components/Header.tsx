404a3fa9769900bd702158d2e5980eb10480f6b6
t4sk <tsk.nakamura@gmail.com>
2024-12-05 13:50:49 +0900

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,8 @@ import styles from "./Header.module.css"
 import DarkMode from "./svg/DarkMode"
 import LightMode from "./svg/LightMode"
 import Solidity from "./svg/Solidity"
+import "./PoweredByCyfrinLogo"
+import PoweredByCyfrinLogo from "./PoweredByCyfrinLogo"
 
 function Header() {
   const { state, setTheme, toggleSideNav } = useAppContext()
@@ -17,18 +19,21 @@ function Header() {
     <div className={styles.component}>
       <Hamburger size={20} className={styles.hamburger} onClick={toggleSideNav} />
       <div className={styles.center}>
-        <a href="/" className={styles.a}>
-          <Solidity
-            className={styles.logo}
-            size={30}
-            fill={state.theme == "dark" ? "rgb(0, 255, 0)" : "currentColor"}
-          />
-        </a>
-        <h3 className={styles.header}>
+        <div className={styles.centerInner}>
           <a href="/" className={styles.a}>
-            Solidity by Example
+            <Solidity
+              className={styles.logo}
+              size={30}
+              fill={state.theme == "dark" ? "rgb(0, 255, 0)" : "currentColor"}
+            />
           </a>
-        </h3>
+          <h3 className={styles.header}>
+            <a href="/" className={styles.a}>
+              Solidity by Example
+            </a>
+          </h3>
+        </div>
+        <PoweredByCyfrinLogo />
       </div>
       <button className={styles.mode} onClick={onClickTheme}>
         {state.theme == "dark" ? <DarkMode size={20} /> : <LightMode size={20} />}
---
+++ src/components/PoweredByCyfrinLogo.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from "./powered-by-cyfrin.png"
+import styles from "./PoweredByCyfrinLogo.module.css"
+
+const PoweredByCyfrinLogo: React.FC<{}> = () => {
+  return (
+    <a target="__blank" href="https://www.cyfrin.io/">
+      <img src={logo} alt="Powered by Cyfrin" className={styles.component} />
+    </a>
+  )
+}
+
+export default PoweredByCyfrinLogo
