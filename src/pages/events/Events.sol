437b1a54c5195f064b1be75eb65c46d549b5dd77
CrazyFrog <anna.shuraeva13@gmail.com>
2024-12-28 10:33:15 +0100

    Update Events.sol

--- src/pages/events/Events.sol
+++ src/pages/events/Events.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.26;
 contract Event {
     // Event declaration
     // Up to 3 parameters can be indexed.
-    // Indexed parameters helps you filter the logs by the indexed parameter
+    // Indexed parameters help you filter the logs by the indexed parameter
     event Log(address indexed sender, string message);
     event AnotherLog();
 
