08d1122cab60897cf4246eaa8a7f77723ef48143
t4sk <tsk.nakamura@gmail.com>
2025-01-21 19:04:00 +0900

    merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,9 @@ import updraft from "../components/updraft.png"
 import { ROUTES, ROUTES_BY_CATEGORY, TRANSLATIONS } from "../nav"
 
 const UPDATES = [
+  "2025/01/21 - GitHub PR by Fallengirl",
+  "2025/01/21 - GitHub PR by XxAlex74xX",
   "2025/01/13 - GitHub PR by Pricstas",
-  "2025/01/13 - GitHub PR by Dahka2321",
-  "2025/01/06 - GitHub PR by Guayaba221",
 ]
 
 export default function HomePage() {
