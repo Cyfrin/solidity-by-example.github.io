b36fd576fd4e6af589e8e232f9e677935ca06ffb
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-03-31 16:15:58 +0200

    Update PoweredByCyfrinLogo.tsx

--- src/components/PoweredByCyfrinLogo.tsx
+++ src/components/PoweredByCyfrinLogo.tsx
@@ -4,7 +4,7 @@ import styles from "./PoweredByCyfrinLogo.module.css"
 
 const PoweredByCyfrinLogo: React.FC<{}> = () => {
   return (
-    <a target="__blank" href="https://www.cyfrin.io/">
+    <a target="_blank" href="https://www.cyfrin.io/">
       <img src={logo} alt="Powered by Cyfrin" className={styles.component} />
     </a>
   )
